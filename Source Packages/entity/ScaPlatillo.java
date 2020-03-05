f75a92169800904d4b3c3e22f5cb5361e7806fb8
MarlonMazariego <61699445+MarlonMazariego@users.noreply.github.com>
2020-03-05 16:32:33 -0600

    Se agrego @Entity
    Falto el @Entity

--- Source Packages/entity/ScaPlatillo.java
+++ Source Packages/entity/ScaPlatillo.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+@Entity
 @Table(name = "tbscaPlatillo")
 public class ScaPlatillo implements Serializable{
   
